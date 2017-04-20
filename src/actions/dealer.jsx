f0bcf2fdb277e629f02d7c5960fcbe4b82bf0e74
requinard <d.diks94@gmail.com>
2017-04-20 18:31:57 +0200

    feat(dealer): cache dealers
    Do not repeat a dealer request if the dealers are already here

--- src/actions/dealer.jsx
+++ src/actions/dealer.jsx
@@ -1,16 +1,23 @@
 export const DEALERS_FETCHING = 'DEALERS_FETCHING'
 export const DEALERS_FETCHED = 'DEALERS_FETCHED'
+export const DEALERS_ALREADY_PRESENT = 'DEALERS_ALREADY_PRESENT'
 import request from 'superagent-es6-promise'
 
 export function fetchDealers () {
-  return (dispatch) => {
-    dispatch({type: DEALERS_FETCHING})
+  return (dispatch, getState) => {
+    if(getState().dealer.dealers.length ===0 ) {
+      dispatch({type: DEALERS_FETCHING})
 
-    return request
-      .get('https://app.eurofurence.org/api/Dealer')
-      .then(result => {
-        dispatch({type: DEALERS_FETCHED, data: result.body})
-      })
-      .catch(error => console.log(error))
+      return request
+        .get('https://app.eurofurence.org/api/Dealer')
+        .then(result => {
+          dispatch({type: DEALERS_FETCHED, data: result.body})
+        })
+        .catch(error => console.log(error))
+    }
+
+    dispatch({type: DEALERS_ALREADY_PRESENT})
+
+    return Promise.resolve()
   }
 }

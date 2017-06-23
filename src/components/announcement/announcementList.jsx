6b67a0b3b8081521554005ecb11b7363bb94ebcb
requinard <d.diks94@gmail.com>
2017-06-23 11:23:54 +0200

    fix(home): add filtering to announcements

--- src/components/announcement/announcementList.jsx
+++ src/components/announcement/announcementList.jsx
@@ -5,11 +5,22 @@ import { Announcement } from './announcement'
 export const AnnouncementList = props => (
   <div>
     {props.announcements.length < 0 ? <h3>There are no announcements!</h3>:
-      props.announcements.map((announcement, key) => <Announcement announcement={announcement} key={key} /> )
+      props.announcements
+        .filter(announcement => {
+          const current = Date.now()
+          const begin = Date.parse(announcement.ValidFromDateTimeUtc)
+          const end = Date.parse(announcement.ValidUntilDateTimeUtc)
+
+          return current > begin && current < end
+        })
+        .map((announcement, key) => <Announcement announcement={announcement} key={key} /> )
     }
   </div>
 )
 
 AnnouncementList.propTypes = {
-  announcements: PropTypes.array
+  announcements: PropTypes.arrayOf(PropTypes.shape({
+    ValidFromDateTimeUtc: PropTypes.string,
+    ValidUntilDateTimeUtc: PropTypes.string
+  }))
 }
